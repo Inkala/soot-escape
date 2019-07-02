d4506fb81e7f3d21d804f5f64b772607a118eab0
Marce Ramirez <marceram87@gmail.com>
2019-07-02 15:19:47 +0200

    Create 3 screens in DOM

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+function main() {
+  var mainElement = document.querySelector('#site-main');
+
+  function buildDom(html) {
+    mainElement.innerHTML = html;
+    return mainElement;
+  }
+
+  function createGameStartScreen() {
+    var gameStartScreen = buildDom(`
+      <section>
+        <h1>Soot Escape</h1>
+        <button>Start!</button>
+      </section>
+    `);
+    var startButton = gameStartScreen.querySelector('button');
+    startButton.addEventListener('click', createGameScreen);
+  }
+
+  function createGameScreen() {
+    var gameScreen = buildDom(`
+      <section>
+        <h1>Soot Escape</h1>
+        <canvas width="600" height="400"></canvas>
+      </section>
+    `);
+    setTimeout(createGameOverScreen, 3000);
+  }
+
+  function createGameOverScreen() {
+    var gameOverScreen = buildDom(`
+      <section>
+        <h1>GAME OVER</h1>
+        <button>Retry?</button>
+      </section>
+    `);
+    var overButton = gameOverScreen.querySelector('button');
+    overButton.addEventListener('click', createGameScreen);
+  }
+
+  createGameStartScreen();
+}
+
+window.addEventListener('load', main);
