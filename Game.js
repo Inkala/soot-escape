5ea3be3aa2368f70a855379354ff44aa1de35ee8
Marce Ramirez <marceram87@gmail.com>
2019-07-02 17:43:24 +0200

    Draw obstacles with space in the middle

--- Game.js
+++ Game.js
@@ -10,15 +10,22 @@ function Game(canvas) {
 
 Game.prototype.startGame = function() {
   this.player = new Player(this.canvas);
-  this.obstacle = new Obstacle(this.canvas);
+  var randomY = Math.random() * (this.canvas.height - 75) + 15;
+
+  // TODO: Add obstacles at regular intervals
+  var topObstacle = new Obstacle(this.canvas, randomY, 1);
+  var bottomObstacle = new Obstacle(this.canvas, (randomY + 60), 2);
+  this.obstacles.push(topObstacle, bottomObstacle);
   var loop = () => {
     this.draw();
     requestAnimationFrame(loop);
-  }
+  };
   loop();
-}
+};
 
 Game.prototype.draw = function() {
   this.player.draw();
-  this.obstacle.draw();
-}
+  this.obstacles.forEach(obstacle => {
+    obstacle.draw();
+  });
+};
--- Obstacle.js
+++ Obstacle.js
@@ -1,26 +1,33 @@
 'use strict';
 
-function Obstacle(canvas) {
+function Obstacle(canvas, randomY, order) {
   this.canvas = canvas;
   this.ctx = canvas.getContext('2d');
 
-  this.width = 45;
-  this.height = 150;
-  this.x = this.canvas.width - this.width;
+  this.width = 35;
+  this.height = randomY;
+  this.x = this.canvas.width - this.width; // Should be 0
   this.y = 0;
   // this.url = null;
-  this.color = "brown";
-  this.direction = 1;
+  this.color = 'sienna';
   this.speed = 3;
+  this.order = order;
 }
 
 Obstacle.prototype.draw = function() {
   this.ctx.fillStyle = this.color;
-  this.ctx.fillRect(this.x, this.y, this.width, this.height);
-}
+  if (this.order === 1) {
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  } else {
+    this.ctx.fillRect(
+      this.x,
+      this.height,
+      this.width,
+      this.canvas.height - this.height
+    );
+  }
+};
 
-Obstacle.prototype.move = function() {
-}
+Obstacle.prototype.move = function() {};
 
-Obstacle.prototype.setDirection = function() {
-}
+Obstacle.prototype.setDirection = function() {};
