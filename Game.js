56acba585cb20156cc6b040b6ed803708fa45b74
Marce Ramirez <marceram87@gmail.com>
2019-07-03 15:13:00 +0200

    Change Obstacle height settings

--- Game.js
+++ Game.js
@@ -1,21 +1,41 @@
 'use strict';
 
 function Game(canvas) {
+  this.canvas = canvas;
+  this.ctx = canvas.getContext('2d');
+
   this.player = null;
   this.obstacles = [];
   this.isGameOver = false;
-  this.canvas = canvas;
-  this.ctx = canvas.getContext('2d');
+  this.obsFrequency = 2000;
+  this.initHeight = 150;
+  this.obsSpace = 100;
+  this.obsVariant = 200;
+  this.obsMinHeight = 30;
 }
 
 Game.prototype.startGame = function() {
   this.player = new Player(this.canvas);
+  var firstTopObstacle = new Obstacle(this.canvas, this.initHeight, 1);
+  var firstBottomObstacle = new Obstacle(
+    this.canvas,
+    this.initHeight + this.obsSpace,
+    2
+  );
+  this.obstacles.push(firstTopObstacle, firstBottomObstacle);
+  var prevHeight = this.initHeight;
   setInterval(() => {
-    var randomY = Math.random() * (this.canvas.height - 130) + 30;
-    var topObstacle = new Obstacle(this.canvas, randomY, 1);
-    var bottomObstacle = new Obstacle(this.canvas, (randomY + 100), 2);
+    var newHeight = this.createObstacleHeight(prevHeight);
+    var topObstacle = new Obstacle(this.canvas, newHeight, 1);
+    var bottomObstacle = new Obstacle(
+      this.canvas,
+      newHeight + this.obsSpace,
+      2,
+      this.canvas.height
+    );
     this.obstacles.push(topObstacle, bottomObstacle);
-  }, 2000);
+    prevHeight = newHeight;
+  }, this.obsFrequency);
   var loop = () => {
     this.player.setBoundaryPosition();
     this.update();
@@ -24,6 +44,23 @@ Game.prototype.startGame = function() {
     requestAnimationFrame(loop);
   };
   loop();
+};
+
+Game.prototype.createObstacleHeight = function(prevHeight) {
+  var minHeight = this.obsMinHeight + this.obsVariant;
+  var maxHeight = this.canvas.height - this.obsMinHeight - this.obsVariant;
+  var randomVariation = Math.random() * this.obsVariant;
+  var direction = 0;
+
+  if (prevHeight < minHeight) {
+    direction = 1;
+  } else if (prevHeight > maxHeight) {
+    direction = -1;
+  } else {
+    direction = Math.floor(Math.random() * 2) * 2 - 1;
+  }
+
+  return prevHeight + randomVariation * direction;
 };
 
 Game.prototype.draw = function() {
@@ -38,8 +75,12 @@ Game.prototype.update = function() {
   this.obstacles.forEach(obstacle => {
     obstacle.move();
   });
-}
+  console.log(this.obstacles.length)
+  if (this.obstacles.length > 10) {
+    this.obstacles.shift();
+  }
+};
 
 Game.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}
+};
--- Obstacle.js
+++ Obstacle.js
@@ -1,35 +1,37 @@
 'use strict';
 
-function Obstacle(canvas, randomY, order) {
+function Obstacle(canvas, height, order) {
   this.canvas = canvas;
   this.ctx = canvas.getContext('2d');
 
   this.width = 25;
-  this.height = randomY;
-  this.x = this.canvas.width
+  this.height = height;
+  this.x = this.canvas.width;
   this.y = 0;
   // this.url = null;
-  this.color = '#57381f';
+  this.color1 = '#57381f';
+  this.color2 = '#362314';
   this.speed = 3;
   this.order = order;
 }
 
 Obstacle.prototype.draw = function() {
-  this.ctx.fillStyle = this.color;
   if (this.order === 1) {
+    this.ctx.fillStyle = this.color1;
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
   } else {
+    this.ctx.fillStyle = this.color2;
     this.ctx.fillRect(
       this.x,
       this.height,
       this.width,
-      this.canvas.height - this.height
+      this.canvas.height - this.height + 100
     );
   }
 };
 
 Obstacle.prototype.move = function() {
-  this.x = this.x - this.speed / 2; 
+  this.x = this.x - this.speed / 2;
 };
 
 Obstacle.prototype.setDirection = function() {};
